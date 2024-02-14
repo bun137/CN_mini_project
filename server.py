85f50f900996cdd5cd0df0379277032488f68470
Shreya Gurram <gurram13775@gmail.com>
2024-02-14 12:13:11 +0530

    adds multi client

--- server.py
+++ server.py
@@ -3,30 +3,45 @@ import cv2
 import pickle
 import struct
 import imutils
+import threading
+
+
+def handle_client(client_socket):
+    vid = cv2.VideoCapture(0)
+    while True:
+        ret, frame = vid.read()
+        if not ret:
+            break  # Break the loop if the video capture fails
+        frame = imutils.resize(frame, width=320)
+        a = pickle.dumps(frame)
+        message = struct.pack("Q", len(a)) + a
+        client_socket.sendall(message)
+
+        cv2.imshow('Sending...', frame)
+        key = cv2.waitKey(10) & 0xFF  # Handle key presses correctly
+        if key == 13:  # ASCII value for enter key
+            break
+    vid.release()  # Release the VideoCapture resource
+    cv2.destroyAllWindows()  # Close all OpenCV windows
+    client_socket.close()  # Close the client socket
+
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_name = socket.gethostname()
 host_ip = '10.1.18.32'
-print('HOST IP:', host_ip)
-port = 10050
+port = 10051
 socket_address = (host_ip, port)
 server_socket.bind(socket_address)
 server_socket.listen(5)
 print("Listening at:", socket_address)
-while True:
-    client_socket, addr = server_socket.accept()
-    print("conection from:", addr)
-    if client_socket:
-        vid = cv2.VideoCapture(0)
-        while (vid.isOpened()):
-            img, frame = vid.read()
-            frame = imutils.resize(frame, width=320)
-            a = pickle.dumps(frame)
-            message = struct.pack("Q", len(a)) + a
-            client_socket.sendall(message)
 
-            cv2.imshow('Sending...', frame)
-            key = cv2.waitKey(10)
-            if key == 13:
-                client_socket.close()
-                break
+try:
+    while True:
+        client_socket, addr = server_socket.accept()
+        print("Connection from:", addr)
+        client_thread = threading.Thread(
+            target=handle_client, args=(client_socket,))
+        client_thread.start()
+except KeyboardInterrupt:
+    print("Server shutting down.")
+finally:
+    server_socket.close()
