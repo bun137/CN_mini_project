f5b0bb2da9d4cd7be1a84d79b0b193bf0e436920
Shreya Gurram <gurram13775@gmail.com>
2024-02-23 14:57:02 +0530

    finallyyyyyyy

--- client.py
+++ client.py
@@ -1,11 +1,12 @@
+
 import socket
 import cv2
 import pickle
 import struct
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = '10.1.18.32'
-port = 10050
+host_ip = "127.0.1.1"
+port = 9994
 client_socket.connect((host_ip, port))
 data = b""
 payload_size = struct.calcsize("Q")
@@ -18,13 +19,22 @@ while True:
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
     msg_size = struct.unpack("Q", packed_msg_size)[0]
+
     while len(data) < msg_size:
         data += client_socket.recv(4*1024)
+
     frame_data = data[:msg_size]
     data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    cv2.imshow("Receiving...", frame)
-    key = cv2.waitKey(10)
-    if key == 13:
+
+    try:
+        frame = pickle.loads(frame_data)
+        cv2.imshow("Received", frame)
+    except Exception as e:
+        print(f"Error deserializing frame data: {e}")
         break
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
 client_socket.close()
